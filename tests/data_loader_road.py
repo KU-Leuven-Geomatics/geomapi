4e78ea89704966c6c6723a4e4a03c97d3d8f9931
maarten bassier <maarten.bassier@kuleuven.be>
2023-11-29 15:10:38 +0100

    update test_node

--- tests/data_loader_road.py
+++ tests/data_loader_road.py
@@ -14,6 +14,7 @@ import time
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
+
 import geomapi.utils as ut
 import geomapi.tools as tl 
 
--- tests/test_node.py
+++ tests/test_node.py
@@ -24,6 +24,7 @@ import geomapi.tools as tl
 from geomapi.nodes import *
 
 #DATA
+sys.path.append(current_dir)
 from data_loader_parking import DataLoaderParking
 from data_loader_road import DataLoaderRoad
 
